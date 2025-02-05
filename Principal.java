48441d87cead2936c8a339121c8b435efae5c12d
davidmmclase <2905829@alu.murciaeduca.es>
2025-02-05 09:36:36 +0000

    Añadida la clase Usuarios y modificado el programa principal para poder crear usuarios y mostrarlos por consola.

--- Principal.java
+++ Principal.java
@@ -1,14 +1,46 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Principal{
         public static void main(String[] arg){
-                System.out.println("¡Hola mundo!");
+                Usuario[] usuarios = añadirUsuarios();
+		System.out.println("¡Hola mundo!");
                 fechaHoraActual();
+		mostrarUsuarios(usuarios);
         }
-        public static void fechaHoraActual() {
+        public static void fechaHoraActual(){
                 LocalDateTime fecha = LocalDateTime.now();
                 DateTimeFormatter formato = DateTimeFormatter.ofPattern("// dd/MM/YYYY \\\\ | ( hh:mm:ss )");
                         System.out.println(fecha.format(formato));
+    	}
+	public static Usuario[] añadirUsuarios(){
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("NÚMERO DE USUARIOS PARA AÑADIR: ");
+		int total = teclado.nextInt();
+
+		teclado.nextLine();
+
+		Usuario[] usuarios = new Usuario[total];
+		for(int i=0; i<total; i++){
+			System.out.println("DATOS DE USUARIOS no." + (i+1) + ("Nombre, apellidos, email): "));
+			String nombre = teclado.nextLine();
+			String apellidos = teclado.nextLine();
+			String email = teclado.nextLine();
+			usuarios[i] = new Usuario(nombre, apellidos, email);
+		}
+	return usuarios;
+	}
+	public static void mostrarUsuarios(Usuario[] usuarios) {
+	System.out.println("\nINFORMACIÓN USUARIOS: ");
+	for(int i=0; i<usuarios.length; i++){
+		System.out.println("Usuario: " + (i + 1) + ": ");
+		System.out.println("Nombre: " + usuarios[i].nombre);
+		System.out.println("Apellidos: " + usuarios[i].apellidos);
+		System.out.println("Email: " + usuarios[i].email);
+		System.out.println();
+        }
     }
 }
+
--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Mostrará por consola un simple "¡Hola mundo!"
 > Practica4_1, principal modificado  
 Mostrará por consola la fecha y hora actual.
 
+> Practica4_1b, principal modificado  
+Creamos tres atributos (nombre, apellidos, email) para hacer un método  
+donde insertamos el número que deseemos de usuarios (objetos ) para añadir la 
+información de cada uno de estos en el array.  
+Mostrará por pantalla posteriormente los usuarios con sus respectivos datos.  
