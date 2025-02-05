02106d78351f8933eaf05bb4186a678538135508
davidmmclase <2905829@alu.murciaeduca.es>
2025-02-05 08:43:00 +0000

    Actualizado Principal.java para mostrar la fecha y hora actual

--- Principal.java
+++ Principal.java
@@ -1,6 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Principal{
-	public static void main(String[] arg){
-		System.out,println("Hola mundo");
-	}
+        public static void main(String[] arg){
+                System.out.println("¡Hola mundo!");
+                fechaHoraActual();
+        }
+        public static void fechaHoraActual() {
+                LocalDateTime fecha = LocalDateTime.now();
+                DateTimeFormatter formato = DateTimeFormatter.ofPattern("// dd/MM/YYYY \\\\ | ( hh:mm:ss )");
+                        System.out.println(fecha.format(formato));
+    }
 }
